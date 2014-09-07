772397852872b00528ee2149a1e494d204154ac2
davidsmith3 <david.smith3@outlook.com>
2014-09-07 18:56:40 -0400

    basic structure added

---
+++ Skyline.Domain/Processor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skyline.Domain {
+    public class Processor {
+
+        public List<Coordinate> GetSkyline(List<Building> buildings) {
+            List<Coordinate> coordinates = new List<Coordinate>();
+
+            return coordinates;
+        }
+    }
+}
---
+++ Skyline.Test/TestProcessor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Skyline.Domain;
+
+namespace Skyline.Test {
+    [TestClass]
+    public class TestProcessor {
+        [TestMethod]
+        public void Test_Empty() {
+            List<Building> buildings = new List<Building>();
+
+            Processor p = new Processor();
+            List<Coordinate> result = p.GetSkyline(buildings);
+
+            Assert.AreEqual(0, result.Count);
+        }
+    }
+}
